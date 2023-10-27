09328b036e96f68e1661fd9a2b2af95c795ec9e5
Eszter <szteran@gmail.com>
2023-10-27 13:43:49 +0200

    header, main

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-console.log("loaded");
+console.log('loaded')
 
-const rootElement = document.querySelector("#root");
-console.log(rootElement); //kilogoltathatom a kulcsait az adatnak
+const rootElement = document.querySelector("#root")
 
 const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json(); //kicsomagolja aválaszból az adatot a jobboldali rész
-  return data;
-};
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+const skeletonComponent = () => `
+    <header></header>
+    <main></main>
+    <footer></footer>
+`
+
+const headerButtonComponent = (buttonText) => `
+    <button id="${buttonText.toLowerCase()}">${buttonText}</button>
+`
 
 const countryComponent = (country) => `
     <div class="country">
         <h2>country name: ${country.name.common}</h2>
         <h3>country pop: ${country.population}</h3>
-   </div>
-    `;
+    </div>
+`
 
-async function init() {
-  // felkészítjük a JS-t, hogy a f-ben lesznek async kódok
-  const data = await fetchUrl("https://restcountries.com/v3.1/all");
-
-  console.log(data);
-
-  data.forEach((country) =>
-    rootElement.insertAdjacentHTML("beforeend", countryComponent(country))
-  );
-
-  const countryElements = document.querySelectorAll("div.country")
-  countryElements.forEach((countryElement) => {
-    countryElement.addEventListener("click", () => {
-        countryElement.classList.toggle("clicked")
-    })
-
-  })
-
+const makeDomFromData = (data, rootElement, componentFn) => {
+    data.forEach(value => rootElement.insertAdjacentHTML("beforeend", componentFn(value)))
 }
 
-init();
+async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    rootElement.insertAdjacentHTML("beforeend", skeletonComponent())
+    const headerElement = document.querySelector("header")
+    const mainElement = document.querySelector("main")
+    const footerElement = document.querySelector("footer")
+
+    makeDomFromData(["Countries", "Swapi", "Contact"], headerElement, headerButtonComponent)
+    const swapiButtonElement = document.querySelector("#swapi")
+    swapiButtonElement.addEventListener("click", () => {
+        mainElement.innerHTML = "Under construction!!!!!!!!!!!!"
+    })
+
+    const data = await fetchUrl("https://restcountries.com/v3.1/all")
+    makeDomFromData(data, mainElement, countryComponent)
+    const countryElements = document.querySelectorAll("div.country")
+    countryElements.forEach((countryElement) => {
+        countryElement.addEventListener("click", () => {
+            countryElement.classList.toggle("clicked")
+        })
+    })
+}
+
+init()
